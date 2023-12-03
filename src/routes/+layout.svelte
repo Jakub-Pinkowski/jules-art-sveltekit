e6eb88f98594123a2bd2c0dcf1a95cb04e6d2e87
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-03 12:42:27 +0100

    Refactor Footer and Layout components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,12 @@
 	import Footer from '$lib/components/Footer.svelte';
 </script>
 
+<div class="flex flex-col min-h-screen">
 <h2>Layout</h2>
-<slot />
+<main class="flex-1 mx-6">
+    <slot />
+</main>
 <Footer></Footer>
+</div>
 
 <style></style>
