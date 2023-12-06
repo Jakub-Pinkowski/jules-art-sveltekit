ee3b99a27ec01b19a3326e0b1ae70af5e47ce63b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-06 21:08:55 +0100

    Add toast notification for successful file download

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -1,10 +1,20 @@
 <script lang="ts">
 	import type { PageData } from './$types';
+    import { fade } from 'svelte/transition';
 
 	export let data: PageData;
+
+	let toastVisible = false;
+
+	const downloadFile = () => {
+		toastVisible = true;
+		setTimeout(() => {
+			toastVisible = false;
+		}, 2000);
+	};
 </script>
 
-<div class="mt-8 w-3/5 fle flex-col">
+<div class="mt-8 w-3/5 fle flex-col mx-auto">
 	{#each data.resources as { name, description, src, poster }}
 		<div class="card card-compact w-full bg-base-100 shadow-xl my-8">
 			<figure>
@@ -17,7 +27,7 @@
 				</p>
 				<div class="card-actions justify-end">
 					<a href={src} download>
-						<button class="btn btn-primary">Download</button>
+						<button class="btn btn-primary" on:click={downloadFile}>Download</button>
 					</a>
 				</div>
 			</div>
@@ -25,4 +35,11 @@
 	{/each}
 
 	<!-- Toast -->
+	{#if toastVisible}
+		<div class="toast toast-top toast-center top-20" transition:fade>
+			<div class="alert alert-success">
+				<span>File downloaded successfully</span>
+			</div>
+		</div>
+	{/if}
 </div>
