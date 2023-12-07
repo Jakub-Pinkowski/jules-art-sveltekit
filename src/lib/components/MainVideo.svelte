ad0bd334402696e5201255d5ba6e7cf783f12929
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-07 21:39:12 +0100

    Add font-awesome dependency and mute button to MainVideo component

--- src/lib/components/MainVideo.svelte
+++ src/lib/components/MainVideo.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
 	import promo from '$lib/assets/movies/Promo.mov';
+
+	const videoElement = <HTMLVideoElement | null>null;
+	let isMuted = false;
+
+	const toggleMute = () => {
+		if (videoElement) {
+			videoElement.muted = !videoElement.muted;
+			isMuted = videoElement.muted;
+		}
+	};
+
+	$: muteIconClass = isMuted ? 'fas fa-volume-mute' : 'fas fa-volume-up';
 </script>
 
 <div>
 	<div>
 		<!-- svelte-ignore a11y-media-has-caption -->
 		<video class="absolute inset-0 w-full h-full object-cover" autoplay muted src={promo}></video>
-		<span class="absolute inset-0 bottom-0 left-0 text-white">SCROLL DOWN</span>
-		<button></button>
+		<span class="absolute bottom-0 left-0 text-white">SCROLL DOWN</span>
+		<button on:click={toggleMute}>
+			<i class={muteIconClass}></i>
+		</button>
 	</div>
 
 	<div class="h-screen"></div>
