ac9d3e150b2346928cbc367adc09f7bf9da0af33
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-09 23:30:52 +0100

    Add social media icons and links to Contact page

--- src/lib/components/Home/Contact.svelte
+++ src/lib/components/Home/Contact.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
 	import passion_photo from '$lib/assets/images/contact.jpg';
+	import insta_icon from '$lib/assets/icons/insta_icon.svg';
+	import tiktok_icon from '$lib/assets/icons/tiktok_icon.svg';
+
+	const insta_link = 'https://www.instagram.com/jules_matias/?hl=en';
+	const tiktok_link = 'https://www.tiktok.com/@jules_matias?_t=8g9XXu8AGnI&_r=1';
 </script>
 
 <div class="p-4 text-justify">
@@ -10,13 +15,32 @@
 	<span class="text-md font-light my-4"
 		>P.S. It goes straight to email box, and I read it every day!</span
 	>
-	<form action="" class="flex flex-col">
-		<input class="input input-primary w-full max-w-sm my-4" type="text" placeholder="Name" />
-		<input class="input input-primary w-full max-w-sm my-4" type="text" placeholder="E-mail" />
-		<textarea class="textarea textarea-primary w-full max-w-sm my-4" placeholder="Message"
+	<form action="" class="flex flex-col mb-8">
+		<input class="input input-primary w-full max-w-md my-4" type="text" placeholder="Name" />
+		<input class="input input-primary w-full max-w-md my-4" type="text" placeholder="E-mail" />
+		<textarea class="textarea textarea-primary w-full max-w-md my-4" placeholder="Message"
 		></textarea>
-		<button class="btn btn-primary w-48" type="submit"> Submit </button>
+		<button class="btn btn-primary w-48 my-4" type="submit"> Submit </button>
 	</form>
+	<span class="text-lg font-light my-4">Let's connect on Social Media</span>
+	<div class="flex">
+		<a href={insta_link}>
+			<img
+				class="w-16 h-16 mr-4 my-4 transition-transform
+                        duration-300 ease-in-out transform hover:scale-110"
+				src={insta_icon}
+				alt="instagram icon"
+			/>
+		</a>
+		<a href={tiktok_link}>
+			<img
+				class="w-16 h-16 mr-4 my-4
+                        transition-transform duration-300 ease-in-out transform hover:scale-110"
+				src={tiktok_icon}
+				alt="tiktok icon"
+			/>
+		</a>
+	</div>
 </div>
 
 <img class="w-full p-4 object-cover rounded-3xl" src={passion_photo} alt="passion let us here" />
