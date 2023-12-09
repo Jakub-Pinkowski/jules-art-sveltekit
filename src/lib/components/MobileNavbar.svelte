7c69d153c1fbe015dfb7e19b7a7107a6bd3ad9b9
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-09 13:07:44 +0100

    Refactor mobile navigation component

--- src/lib/components/MobileNavbar.svelte
+++ src/lib/components/MobileNavbar.svelte
@@ -1,90 +1,72 @@
-<script>
-	let isActive = false;
+<script lang="ts">
+	import { fly, scale } from 'svelte/transition';
+	import { quadOut } from 'svelte/easing';
 
-	const toggle = () => {
-		isActive = !isActive;
+	export let open: boolean;
+
+	const links = [
+		{ label: 'Home', path: '/' },
+		{ label: 'Photos', path: '/photos' },
+		{ label: 'Movies', path: '/movies' },
+		{ label: 'Reels', path: '/reels' },
+		{ label: 'Resources', path: '/resources' }
+	];
+
+	const closeMenu = () => {
+		open = false;
 	};
 </script>
 
-<!-- TODO: Finish the navbar -->
-<nav class="md:hidden">
-	<button class={`hamb ${isActive ? 'active' : ''}`} aria-label="Open Menu" on:click={toggle}>
-		<span class="sr-only">Open Menu</span><svg class="ham" viewBox="0 0 100 100"
-			><path
-				class="line top"
-				d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
-			></path><path class="line middle" d="m 30,50 h 40"></path><path
-				class="line bottom"
-				d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
-			></path></svg
-		></button
-	>
-</nav>
+{#if open}
+	<div class="text-center text-semibold text-2xl p-4 pt-0 tracking-wider">
+		<a
+			href={links[0].path}
+			class="block cursor-pointer mx-auto my-4 hover:underline"
+			on:click={closeMenu}
+			transition:fly={{ y: -15, delay: 50 }}
+		>
+			{links[0].label}
+		</a>
 
-<style lang="scss">
-	button {
-		color: black;
-	}
-	.hamb {
-		position: relative;
-		margin-right: -0.625rem;
-		border-width: 0px;
-		background-color: transparent;
-		padding: 0;
-	}
-	.sr-only {
-		position: absolute;
-		width: 1px;
-		height: 1px;
-		padding: 0;
-		margin: -1px;
-		overflow: hidden;
-		clip: rect(0, 0, 0, 0);
-		white-space: nowrap;
-		border-width: 0;
-	}
-	.hamb .ham {
-		-webkit-tap-highlight-color: transparent;
-		-webkit-user-select: none;
-		-moz-user-select: none;
-		user-select: none;
-	}
-	.hamb .ham {
-		height: 60px;
-		width: 60px;
-		cursor: pointer;
-		transition-duration: 0.3s;
-	}
-	.hamb .ham .top {
-		stroke-dasharray: 40 160;
-	}
-	.hamb .ham .middle {
-		transform-origin: 50%;
-		stroke-dasharray: 40 142;
-	}
-	.hamb .ham .bottom {
-		transform-origin: 50%;
-		stroke-dasharray: 40 85;
-	}
-	.hamb .line {
-		fill: none;
-		stroke: black;
-		stroke-width: 5;
-		transition-duration: 0.3s;
-		stroke-linecap: round;
-	}
-	.hamb.active svg {
-		--tw-rotate: 45deg;
-		transform: rotate(var(--tw-rotate));
-	}
-	.hamb.active svg .top {
-		stroke-dashoffset: -64px;
-	}
-	.hamb.active svg .middle {
-		--tw-rotate: 90deg;
-		transform: rotate(var(--tw-rotate));
-	}
-	.hamb.active svg .bottom {
-		stroke-dashoffset: -64px;
-	}
-</style>
+		<a
+			href={links[1].path}
+			class="block cursor-pointer mx-auto my-4 hover:underline"
+			on:click={closeMenu}
+			transition:fly={{ y: -15, delay: 100 }}
+		>
+			{links[1].label}
+		</a>
+
+		<a
+			href={links[2].path}
+			class="block cursor-pointer mx-auto my-4 hover:underline"
+			on:click={closeMenu}
+			transition:fly={{ y: -15, delay: 150 }}
+		>
+			{links[2].label}
+		</a>
+
+		<a
+			href={links[3].path}
+			class="block cursor-pointer mx-auto my-4 hover:underline"
+			on:click={closeMenu}
+			transition:fly={{ y: -15, delay: 200 }}
+		>
+			{links[3].label}
+		</a>
+
+		<a
+			href={links[4].path}
+			class="block cursor-pointer mx-auto my-4 hover:underline"
+			on:click={closeMenu}
+			transition:fly={{ y: -15, delay: 250 }}
+		>
+			{links[4].label}
+		</a>
+	</div>
+
+	<div
+		class="border-solid border-primary border-t-2 h-0 mb-8"
+		in:scale={{ duration: 400, easing: quadOut, opacity: 1 }}
+	/>
+{/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
 	import Footer from '$lib/components/Footer.svelte';
 	import DesktopNavbar from '$lib/components/DesktopNavbar.svelte';
 	import MobileNavbar from '$lib/components/MobileNavbar.svelte';
-	import MobileNavbar2 from '$lib/components/MobileNavbar2.svelte';
 
 	const primary = '#c8b6ff';
 	let open: boolean;
@@ -14,9 +13,8 @@
 <div class="flex flex-col min-h-screen">
 	<header>
 		<DesktopNavbar />
-		<MobileNavbar />
 		<Hamburger bind:open --color={primary} />
-		<MobileNavbar2 bind:open/>
+		<MobileNavbar bind:open/>
 	</header>
 	<main class="flex-1 mx-6 text-gray-900">
 		<slot />
