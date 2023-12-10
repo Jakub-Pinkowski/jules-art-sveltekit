ae69714458a411093809e0ad267f4ca5e8d439ac
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-10 19:36:49 +0100

    Fix margin/padding issue in movies page

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -3,11 +3,12 @@
 
 	export let data: PageData;
 </script>
+
 <!-- TODO: Fix different margin/padding between this and photos -->
 <div class="p-4">
-	<h2 class=" my-8 text-3xl font-medium md:my-4">Movies</h2>
+	<h2 class=" my-8 pl-4 text-3xl font-medium md:my-4">Movies</h2>
 	{#each data.movies as { name, src, poster }}
-		<div class="flex h-auto w-full flex-col items-center justify-center">
+		<div class="flex h-auto w-full flex-col items-center justify-center p-4">
 			<!-- svelte-ignore a11y-media-has-caption -->
 			<video class=" rounded-lg" controls {src} {poster} />
 			<h3 class="mb-8 mt-4 text-2xl font-medium">{name}</h3>
