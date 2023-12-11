925bda0eeb62c338d53c9a279756d5f2a2d313b5
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-11 20:21:53 +0100

    Add name overlay to carousel items

--- src/lib/components/DesktopSwiper.svelte
+++ src/lib/components/DesktopSwiper.svelte
@@ -7,9 +7,14 @@
 <div class="hidden h-screen md:block">
 	<div class="carousel carousel-center h-4/5 space-x-4 rounded-box bg-neutral p-4">
 		{#each reels as { name, src, poster }, i (name)}
-			<div class="carousel-item">
+			<div id={name} class="carousel-item relative px-8">
 				<!-- svelte-ignore a11y-media-has-caption -->
 				<video class="h-full w-full rounded-box object-cover" {src} {poster} controls></video>
+				<span
+					class="absolute bottom-24 left-0 right-0 z-10 p-2 text-center text-2xl font-medium text-white"
+				>
+					{name}
+				</span>
 			</div>
 		{/each}
 	</div>
