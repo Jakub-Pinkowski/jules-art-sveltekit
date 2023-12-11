25ccfa5bf8c69ba5c1d80795b66c8030fc21c67c
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-11 20:16:18 +0100

    Update DesktopSwiper component to display carousel of videos

--- src/lib/components/DesktopSwiper.svelte
+++ src/lib/components/DesktopSwiper.svelte
@@ -4,6 +4,13 @@
 	const { reels } = data;
 </script>
 
-<div class="hidden md:block">
-	<h1>Desktop</h1>
+<div class="hidden h-screen md:block">
+	<div class="carousel carousel-center h-4/5 space-x-4 rounded-box bg-neutral p-4">
+		{#each reels as { name, src, poster }, i (name)}
+			<div class="carousel-item">
+				<!-- svelte-ignore a11y-media-has-caption -->
+				<video class="h-full w-full rounded-box object-cover" {src} {poster} controls></video>
+			</div>
+		{/each}
+	</div>
 </div>
