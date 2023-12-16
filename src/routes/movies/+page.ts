f49fbd4fdd3ae54b9f5523732391caeeaf96d93b
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-16 20:29:45 +0100

    Remove unused movie assets and commented code

--- src/routes/movies/+page.ts
+++ src/routes/movies/+page.ts
@@ -1,11 +1,5 @@
 import type { PageLoad } from './$types';
 
-// import berlin_romance_1 from '$lib/assets/movies/Berlin_Romance_1.mov';
-// import berlin_romance_2 from '$lib/assets/movies/Berlin_Romance_2.mov';
-
-// import berlin_romance_1_poster from '$lib/assets/movies/Berlin_Romance_1.jpg';
-// import berlin_romance_2_poster from '$lib/assets/movies/Berlin_Romance_2.jpg';
-
 interface movieObject {
 	id: number;
 	name: string;
@@ -28,19 +22,7 @@ export const load = (async () => {
 			{
 				id: 3,
 				name: 'Jane',
-                src: 'https://www.youtube.com/embed/5aVlBcXR-l8?si=hxB2kRrPNNnl0a-2'
-			},
-			{
-				id: 1,
-				name: 'Berlin Romance 1',
-				// src: berlin_romance_1,
-				// poster: berlin_romance_1_poster
-			},
-			{
-				id: 2,
-				name: 'Berlin Romance 2',
-				// src: berlin_romance_2,
-				// poster: berlin_romance_2_poster
+				src: 'https://www.youtube.com/embed/5aVlBcXR-l8?si=hxB2kRrPNNnl0a-2'
 			}
 		] as movieObject[]
 	};
