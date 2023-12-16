05daf33ff1082263bf2ffde93cc2e0f43165175f
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-16 12:18:36 +0100

    Refactor movie display layout in movies page

--- src/routes/movies/+page.svelte
+++ src/routes/movies/+page.svelte
@@ -5,13 +5,15 @@
 </script>
 
 <!-- TODO: Fix different margin/padding between this and photos -->
-<div class="p-4">
-	<h2 class=" my-8 pl-4 text-3xl font-medium md:my-4">Movies</h2>
-	{#each data.movies as { name, src, poster }}
-		<div class="flex h-auto w-full flex-col items-center justify-center p-4">
-			<!-- svelte-ignore a11y-media-has-caption -->
-			<video class=" rounded-lg" controls {src} {poster} />
-			<h3 class="mb-8 mt-4 text-2xl font-medium">{name}</h3>
-		</div>
-	{/each}
+<div class=" p-4">
+	<h2 class="my-8 pl-4 text-3xl font-medium md:my-4">Movies</h2>
+	<div class="flex w-full flex-col items-center">
+		{#each data.movies as { name, src, poster }}
+			<div class="flex h-auto w-10/12 flex-col items-center justify-center md:p-4">
+				<!-- svelte-ignore a11y-media-has-caption -->
+				<video class=" rounded-lg" controls {src} {poster} />
+				<h3 class="mb-8 mt-4 text-2xl font-medium">{name}</h3>
+			</div>
+		{/each}
+	</div>
 </div>
