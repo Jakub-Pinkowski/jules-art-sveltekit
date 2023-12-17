def845fc48a1038d2156e128395d94b5236cd7bc
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-17 15:08:38 +0100

    Add AWS S3 client and list objects command

--- src/routes/reels/+page.server.ts
+++ src/routes/reels/+page.server.ts
@@ -1,16 +1,25 @@
 import type { PageServerLoad } from './$types';
+import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
 
 const AWS_ACCESS_KEY = import.meta.env.VITE_AWS_ACCESS_KEY;
 const AWS_SECRET_ACCESS_KEY = import.meta.env.VITE_AWS_SECRET_ACCESS_KEY;
+
+console.log('AWS_ACCESS_KEY', AWS_ACCESS_KEY);
+console.log('AWS_SECRET_ACCESS_KEY', AWS_SECRET_ACCESS_KEY);
+
 const region = 'eu-central-1';
-
-const bucketName = 'reels';
-
 const params = {
-	Bucket: bucketName,
+	Bucket: 'jules-art',
 	Prefix: 'reels/'
 };
 
+const s3Client = new S3Client({
+	region,
+	credentials: {
+		accessKeyId: AWS_ACCESS_KEY,
+		secretAccessKey: AWS_SECRET_ACCESS_KEY
+	}
+});
 
 interface reelObject {
 	name: string;
@@ -19,5 +28,12 @@ interface reelObject {
 }
 
 export const load = (async () => {
+	const listObjectsCommand = new ListObjectsV2Command(params);
+
+	const { Contents: objects } = await s3Client.send(listObjectsCommand);
+	console.log('objects', objects);
+
+	const reels: reelObject[] = [];
+
 	return {};
 }) satisfies PageServerLoad;
