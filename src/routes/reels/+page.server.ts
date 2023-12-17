a4d6d8a34c8b9bdd0c50664c349d33faad703009
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-17 16:18:39 +0100

    Update reels data and remove unused imports

--- src/routes/reels/+page.server.ts
+++ src/routes/reels/+page.server.ts
@@ -1,4 +1,5 @@
 import type { PageServerLoad } from './$types';
+import { Readable } from 'stream';
 import { S3Client, ListObjectsV2Command, GetObjectCommand } from '@aws-sdk/client-s3';
 
 const AWS_ACCESS_KEY = import.meta.env.VITE_AWS_ACCESS_KEY;
@@ -23,18 +24,49 @@ const s3Client = new S3Client({
 
 interface reelObject {
 	name: string;
+	title: string;
 	src: string;
 	poster: string;
 }
 
-const reelNames: string[] = [
-	'One evening in Paris',
-	'Sunlight',
-	'Your scent',
-	'Mon Paris',
-	'Time for the sun',
-	'Sunny days'
-];
+let reels = [
+	{
+		name: 'reel_1',
+		title: 'One evening in Paris',
+		src: '',
+		poster: ''
+	},
+	{
+		name: 'reel_2',
+		title: 'Sunlight',
+		src: '',
+		poster: ''
+	},
+	{
+		name: 'reel_3',
+		title: 'Your scent',
+		src: '',
+		poster: ''
+	},
+	{
+		name: 'reel_4',
+		title: 'Mon Paris',
+		src: '',
+		poster: ''
+	},
+	{
+		name: 'reel_5',
+		title: 'Time for the sun',
+		src: '',
+		poster: ''
+	},
+	{
+		name: 'reel_6',
+		title: 'Sunny days',
+		src: '',
+		poster: ''
+	}
+] as reelObject[];
 
 export const load = (async () => {
 	const listObjectsCommand = new ListObjectsV2Command(params);
@@ -42,7 +74,9 @@ export const load = (async () => {
 	const { Contents: objects } = await s3Client.send(listObjectsCommand);
 	console.log('objects', objects);
 
-	const reels: reelObject[] = [];
+    
 
-	return {};
+	return {
+		reels
+	};
 }) satisfies PageServerLoad;
