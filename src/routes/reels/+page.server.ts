1b6711d2d62027cbea062de32936fe472946fce2
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-17 14:45:33 +0100

    Add AWS SDK dependency and configure AWS credentials

--- src/routes/reels/+page.server.ts
+++ src/routes/reels/+page.server.ts
@@ -1,4 +1,15 @@
 import type { PageServerLoad } from './$types';
+import AWS from 'aws-sdk';
+
+const AWS_ACCESS_KEY = import.meta.env.VITE_AWS_ACCESS_KEY
+const AWS_SECRET_ACCESS_KEY = import.meta.env.VITE_AWS_SECRET_ACCESS_KEY
+
+console.log('AWS_ACCESS_KEY', AWS_ACCESS_KEY);
+console.log('AWS_SECRET_ACCESS_KEY', AWS_SECRET_ACCESS_KEY);
+
+const region = 'eu-central-1';
+const bucketName = 'reels';
+const s3 = new AWS.S3();
 
 interface reelObject {
 	name: string;
