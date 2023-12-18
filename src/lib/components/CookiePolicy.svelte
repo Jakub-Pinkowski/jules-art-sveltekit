7a55a0fba66245263687a7e4a2ccc5ba78144117
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-18 21:06:05 +0100

    Add acceptAll and rejectAll functionality to CookiePolicy component

--- src/lib/components/CookiePolicy.svelte
+++ src/lib/components/CookiePolicy.svelte
@@ -7,22 +7,33 @@
 			localStorage.setItem('cookieBannerDisplayed', 'true');
 		}
 	}
+
+	const acceptAll = () => {
+		localStorage.setItem('cookieBannerDisplayed', 'true');
+		showBanner = false;
+	};
+
+	// TODO: Implement this
+	const rejectAll = () => {
+		localStorage.setItem('cookieBannerDisplayed', 'true');
+		showBanner = false;
+	};
 </script>
 
 {#if showBanner}
-<div class="card w-[25rem] bg-base-100 shadow-xl">
-	<div class="card-body">
-		<h2 class="card-title">Cookies!</h2>
-		<p>
-			We use cookies to anylyze our traffic. By clicking "Accept All" you consent to our use of
-			cookies
-		</p>
-		<a> Cookie Policy </a>
-		<div class="card-actions">
-			<button class="btn btn-ghost">Customize</button>
-			<button class="btn btn-ghost">Reject All</button>
-			<button class="btn btn-primary">Accept All</button>
+	<div class="card w-[25rem] bg-base-100 shadow-xl">
+		<div class="card-body">
+			<h2 class="card-title">Cookies!</h2>
+			<p>
+				We use cookies to anylyze our traffic. By clicking "Accept All" you consent to our use of
+				cookies
+			</p>
+			<a> Cookie Policy </a>
+			<div class="card-actions">
+				<button class="btn btn-ghost">Customize</button>
+				<button class="btn btn-ghost" on:click={rejectAll}>Reject All</button>
+				<button class="btn btn-primary" on:click={acceptAll}>Accept All</button>
+			</div>
 		</div>
 	</div>
-</div>
 {/if}
