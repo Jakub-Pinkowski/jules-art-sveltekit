7a00651700a5e445994608fc2174f69cc4251a5d
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-18 21:31:06 +0100

    Refactor CookieBanner component layout

--- src/lib/components/CookieBanner.svelte
+++ src/lib/components/CookieBanner.svelte
@@ -15,7 +15,11 @@
 
 	// TODO: Implement this
 	const rejectAll = () => {
+		document.cookie = 'theme=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        console.log('document.cookie:', document.cookie);
+
 		localStorage.setItem('cookieBannerDisplayed', 'true');
+
 		showBanner = false;
 	};
 
@@ -25,21 +29,18 @@
 	};
 </script>
 
-	<div class="card w-[25rem] bg-base-100 shadow-xl">
-		<div class="card-body">
-			<h2 class="card-title">Cookies!</h2>
-			<p>
-				We use cookies to anylyze our traffic. By clicking "Accept All" you consent to our use of
-				cookies
-			</p>
-			<a 
-                class="text-blue-500"
-            href="/cookies"> Cookie Policy </a>
-			<div class="card-actions">
-				<button class="btn btn-ghost" on:click={customizeCookies}>Customize</button>
-				<button class="btn btn-ghost" on:click={rejectAll}>Reject All</button>
-				<button class="btn btn-primary" on:click={acceptAll}>Accept All</button>
-			</div>
+<div class="card w-[25rem] bg-base-100 shadow-xl">
+	<div class="card-body">
+		<h2 class="card-title">Cookies!</h2>
+		<p>
+			We use cookies to anylyze our traffic. By clicking "Accept All" you consent to our use of
+			cookies
+		</p>
+		<a class="text-blue-500" href="/cookies"> Cookie Policy </a>
+		<div class="card-actions">
+			<button class="btn btn-ghost" on:click={customizeCookies}>Customize</button>
+			<button class="btn btn-ghost" on:click={rejectAll}>Reject All</button>
+			<button class="btn btn-primary" on:click={acceptAll}>Accept All</button>
 		</div>
 	</div>
-
+</div>
