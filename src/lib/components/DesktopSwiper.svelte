5184aa32ccc2438b51f01fee34a9ca65b3a4e500
Jakub_Pinkowski <pinkowski.jakub@gmail.com>
2023-12-18 20:44:09 +0100

    Update carousel item IDs to use index

--- src/lib/components/DesktopSwiper.svelte
+++ src/lib/components/DesktopSwiper.svelte
@@ -8,7 +8,7 @@
 <div class="hidden h-screen md:block">
 	<div class="carousel carousel-center h-4/5 space-x-4 rounded-box bg-neutral p-4">
 		{#each reels as { title, src, poster }, i (title)}
-			<div id={src} class="carousel-item relative px-8 pt-4">
+			<div id={i.toString()} class="carousel-item relative px-8 pt-4">
 				<!-- svelte-ignore a11y-media-has-caption -->
 				<video class="h-full w-full rounded-box object-cover" {src} {poster} controls></video>
 				<span
@@ -22,7 +22,7 @@
 	<div class="mt-4 flex w-full justify-center gap-4 p-4">
 		{#each reels as { title, src }, i (title)}
 			<!-- svelte-ignore a11y-missing-content -->
-			<a href={`#${src}`} class="btn btn-primary px-8">{i + 1}</a>
+			<a href={`#${i}`} class="btn btn-primary px-8">{i + 1}</a>
 		{/each}
 	</div>
 </div>
